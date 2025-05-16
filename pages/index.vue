149d29ddca94c9bac633a32958c86ad9ced857c6
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-16 12:13:39 +0000

    Added base page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { createClient } from '@supabase/supabase-js'
+const config = useRuntimeConfig()
+const supabase = createClient(config.public.supabaseUrl, config.public.supabaseAnonKey)
+const filaments = ref([])
+async function getfilaments() {
+  const { data } = await supabase.from('filaments').select()
+  filaments.value = data
+}
+onMounted(() => {
+  getfilaments()
+})
+</script>
 <template>
-  <h1>Index page</h1>
+  <ul>
+    <li v-for="filament in filaments" :key="filament.id">{{ filament.type }}</li>
+  </ul>
 </template>
