a50267f1248a087dfe244d9c0ae912339f837237
Flo <93609174+Xeferis@users.noreply.github.com>
2025-05-17 08:42:51 +0200

    Added Supabase Key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/supabase','@nuxt/ui'],
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxtjs/supabase", "@nuxt/ui"],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+    },
+  },
   supabase: {
     redirectOptions: {
-      login: '/login',
-      callback: '/confirm',
+      login: "/login",
+      callback: "/confirm",
       include: undefined,
       exclude: [],
       saveRedirectToCookie: true,
     },
   },
-})
+});
