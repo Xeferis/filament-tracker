a4211fbcb0f5b0f7288259a1fbeedafca2346b12
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-19 13:06:18 +0000

    Added loading

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,8 @@
 const config = useRuntimeConfig()
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
-const filaments = ref([])
 
-const { data, error } = await supabase.from('filaments').select("id, type, amount, refill, manufacturer, location")
-filaments.value = data
-
+const { data, error, statusText } = await supabase.from('filaments').select("id, type, amount, refill, manufacturer, location")
 
 const LogOut = async () => {
   console.log("sign out user")
@@ -26,26 +23,6 @@ const LogOut = async () => {
   </div>
   <UButton @click="LogOut">LogOut</UButton>
   <div class="flex flex-col justify-center items-center w-full">
-    <table class="border-collapse border border-gray-400 table-auto">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Hersteller</th>
-          <th>refill</th>
-          <th>Amount</th>
-          <th>Location</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="fil in filaments">
-          <td>{{fil.type}}</td>
-          <td>{{fil.manufacturer}}</td>
-          <td>{{fil.refill}}</td>
-          <td>{{fil.amount}}</td>
-          <td>{{fil.location}}</td>
-        </tr>
-      </tbody>
-    </table>
-    <UTable :data="data" />
+    <UTable :data="data" :loading="statusText === 'pending'" />
   </div>
 </template>
