b13608a9d27b675f2affefe9328df11b7d200039
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-19 14:00:43 +0000

    Removed Location from dataset

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 const supabase = useSupabaseClient()
-const { data, status } = await supabase.from('filaments').select("id, type, amount, refill, manufacturer, location")
+const { data, status } = await supabase.from('filaments').select("id, type, amount, refill, manufacturer")
 </script>
 <template>
   <div class="flex flex-col justify-center items-center p-10 w-full">
