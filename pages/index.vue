2408b54bdd867ecaafa0517d889a5fa68ffda4e9
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-20 05:25:31 +0000

    Added ADD Button

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,12 @@ const { data, status } = await supabase.from('filaments').select("id, type, amou
 </script>
 <template>
   <div class="flex flex-col justify-center items-center p-10 w-full">
-    <h1 class="text-6xl text-center mb-16">Current Filament Supply</h1>
-    <UTable :data="data" class="flex-1" />
+    <h1 class="text-6xl text-center mb-14">Current Filament Supply</h1>
+    <div class="flex flex-col w-4/5 md:w-full">
+      <div class="flex justify-end items-center mb-2">
+        <UButton to="/add">Add</UButton>
+      </div>
+      <UTable :data="data" class="flex-1" />
+    </div>
   </div>
 </template>
