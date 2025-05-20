239f6285f7921693cf364e0017758b9e8a12d3cf
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-20 08:39:17 +0000

    Added value

--- pages/add.vue
+++ pages/add.vue
@@ -50,7 +50,7 @@ const addFilament = async () => {
           v-model="manufacturer"
         />
         <UCheckbox class="my-2" v-model="refill" name="refill" label="Refill" />
-        <UInputMenu class="my-2" v-model="dd_value[0]" :items="dd_value" value-key="id" lable-key="description"/>
+        <UInputMenu class="my-2" v-model="dd_value[0]" :items="dd_value"/>
         <UButton size="xl" class="mt-5 justify-center" @click="addFilament">
           Add Filament
         </UButton>
