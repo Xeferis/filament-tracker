0d940381dd9c0aa6796708ea0532a06cb0b7bcc9
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-20 10:29:30 +0000

    Added row styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,51 @@
-<script setup>
+<script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui'
+import { h, resolveComponent } from 'vue'
+const UBadge = resolveComponent('UBadge')
 const supabase = useSupabaseClient()
 const { data, status } = await supabase.from('filaments').select("id, type, amount, refill, manufacturer")
+
+type filament = {
+  id: String
+  type: String
+  amount: Number
+  refill: Boolean
+  manufacturer: String
+}
+
+const columns: TableColumn<filament>[] = [
+  {
+    accessorKey: 'id',
+    header: '#',
+    cell: ({ row }) => `#${row.getValue('id')}`
+  },
+  {
+    accessorKey: 'type',
+    header: 'Bezeichnung',
+  },
+  {
+    accessorKey: 'amount',
+    header: 'Anzahl'
+  },
+  {
+    accessorKey: 'refill',
+    header: 'Refill Roll',
+    cell: ({ row }) => {
+      const color = {
+        true: 'success' as const,
+        false: 'error' as const
+      }[row.getValue('refill') as string]
+
+      return h(UBadge, { class: 'capitalize', variant: 'subtle', color }, () =>
+        row.getValue('refill')
+      )
+    }
+  },
+  {
+    accessorKey: 'manufacturer',
+    header: 'Hersteller'
+  },
+]
 </script>
 <template>
   <div class="flex flex-col justify-center items-center p-10 w-full">
@@ -9,7 +54,7 @@ const { data, status } = await supabase.from('filaments').select("id, type, amou
       <div class="flex justify-end items-center mb-2">
         <UButton to="/add">Add</UButton>
       </div>
-      <UTable :data="data" class="flex-1" />
+      <UTable :data="data" :columns="columns" class="flex-1" />
     </div>
   </div>
 </template>
